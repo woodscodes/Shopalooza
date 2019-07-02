c13c16d56e3ba5ea052c624d74d78366e0d5d14e
stuwo <stooberto@gmail.com>
2019-07-02 20:07:05 +0900

    Initial setup

---
+++ Shopalooza/Shopalooza.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shopalooza.WebUI.MvcApplication" Language="C#" %>
---
+++ Shopalooza/Shopalooza.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shopalooza.WebUI.Startup))]
+namespace Shopalooza.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
