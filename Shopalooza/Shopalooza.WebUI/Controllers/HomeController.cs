2a965f47452ee2f3bb9a5b343ed5931ba4bc113a
stuwo <stooberto@gmail.com>
2019-07-09 20:30:09 +0900

    Added product listing and product details view

--- Shopalooza/Shopalooza.WebUI/Controllers/HomeController.cs
+++ Shopalooza/Shopalooza.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Shopalooza.Core.Contracts;
+using Shopalooza.Core.Models;
+using Shopalooza.DataAccess.SQL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,29 @@ namespace Shopalooza.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private IRepository<Product> _context;
+        private IRepository<ProductCategory> _productCategoryContext;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            _context = productContext;
+            _productCategoryContext = productCategoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var products = _context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string id)
+        {
+            var product = _context.Find(id);
+
+            if (product == null)
+                return HttpNotFound();
+
+            return View(product);
         }
 
         public ActionResult About()
