8a3fa189513a1185cb86a0a160f8d6354cd4ecd0
stuwo <stooberto@gmail.com>
2019-07-09 19:21:00 +0900

    Added SQL repo and updated DI

---
+++ Shopalooza/Shopalooza.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,58 @@
+﻿using Shopalooza.Core.Contracts;
+using Shopalooza.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopalooza.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private DataContext _context;
+        private DbSet<T> _dbSet;
+
+        public SQLRepository(DataContext dataContext)
+        {
+            _context = dataContext;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return _dbSet;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var tToDelete = Find(id);
+            if (_context.Entry(tToDelete).State == EntityState.Detached)
+                _dbSet.Attach(tToDelete);
+
+            _dbSet.Remove(tToDelete);
+        }
+
+        public T Find(string id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Insert(T t)
+        {
+            _dbSet.Add(t);
+        }
+
+        public void Update(T t, string id)
+        {
+            _dbSet.Attach(t);
+            _context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
--- Shopalooza/Shopalooza.WebUI/App_Start/UnityConfig.cs
+++ Shopalooza/Shopalooza.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using Shopalooza.Core.Contracts;
 using Shopalooza.Core.Models;
 using Shopalooza.DataAccess.InMemory;
+using Shopalooza.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace Shopalooza.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
 
         }
     }
